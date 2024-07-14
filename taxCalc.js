fe07d18b9ab515bba9ae0583ff7c547d6aa06946
KIGMAN <Valentine Kiguli>
2024-07-14 13:46:04 -0700

    Third passing test

--- taxCalc.js
+++ taxCalc.js
@@ -3,7 +3,8 @@
 // then with earnings greater than 36000 per year, pay 40% tax
 
 export function taxCalc(a){ // 'a' refers to earnings
-    return (a < 12000) ? 0 : 0.2*a // If earnings are less than 1200 per year, return 0, otherwise return 20% of earnings
+    return (a < 12000) ? 0 : // If earnings < 12000 per year, return 0, otherwise 
+    (a >= 12000 & a < 36000) ? 0.2 * a : 0.4 * a // If earnings > 12000 but < 36000, return 20% of earnings, otherwise return 40% of earnings
 }
 
-// console.log(taxCalc(12000));
+// console.log(taxCalc(40000));
--- test/taxCalcTest.js
+++ test/taxCalcTest.js
@@ -8,4 +8,7 @@ describe('Tax calculator function', function(){
     it('Second tax bracket', function(){
         assert.equal(taxCalc(13000), 2600, 'taxes on 13000 = 2600' );
     })
+    it('third tax bracket', function(){
+        assert.equal(taxCalc(40000), 16000, 'taxes on 40000 = 16000' );
+    })
 })
