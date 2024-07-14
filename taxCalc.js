04493fcae6b97fc4061f641fb51c75679fd6415b
KIGMAN <Valentine Kiguli>
2024-07-14 14:11:49 -0700

    Removed redundant comments

--- taxCalc.js
+++ taxCalc.js
@@ -1,7 +1,3 @@
-// If earnings < 12000 per year, pay no tax
-// then with earnings between 12,000 and 36,000 pay 20% tax
-// then with earnings greater than 36000 per year, pay 40% tax
-
 export function taxCalc(a){ // 'a' refers to earnings
     return (a > 0 & a < 12000) ? 0 : // If earnings > 0 but < 12000 per year, return 0, otherwise 
     (a >= 12000 & a < 36000) ? 0.2 * a : // If earnings > or = 12000 but < 36000, return 20% of earnings, otherwise return 40% of earnings
@@ -9,3 +5,5 @@ export function taxCalc(a){ // 'a' refers to earnings
 }
 
 // console.log(taxCalc(-10000));
+
+// By Valentine Kiguli
